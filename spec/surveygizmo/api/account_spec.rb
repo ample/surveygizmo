1ff5262af42f28058d0e06bebbccedb0bcfbf73b
Steven Chau <steven.chau@mac.com>
2012-09-03 15:14:14 -0700

    Create and attach auth query hash if defined

--- spec/surveygizmo/api/account_spec.rb
+++ spec/surveygizmo/api/account_spec.rb
@@ -2,22 +2,17 @@ require 'helper'
 
 describe Surveygizmo::API do
   before do
-    @client = Surveygizmo::Client.new(
-      :username => "steven@somewhere.org",
-      :password => "keyboardcat"
-    )
+    @client = Surveygizmo::Client.new
   end
 
   describe "#account" do
     before do
      stub_get("/v1/account").
-       with(:query => {:"user:md5" => "steven@somewhere.org:39f8d5313141b1f2bade311ba571537e"}).
        to_return(:body => fixture("account.json"), :headers => {:content_type => "application/json; charset=utf-8"})
     end
     it "requests the correct resource" do
       @client.account
       a_get("/v1/account").
-        with(:query => {:"user:md5" => "steven@somewhere.org:39f8d5313141b1f2bade311ba571537e"}).
         should have_been_made
     end
   end
--- spec/surveygizmo/client_spec.rb
+++ spec/surveygizmo/client_spec.rb
@@ -1,6 +1,13 @@
 require 'helper'
 
 describe Surveygizmo::Client do
+  subject do
+    Surveygizmo::Client.new(
+      :username => "steven@somewhere.org",
+      :password => "keyboardcat"
+    )
+  end
+
   describe "#credentials?" do
     it "returns true if all credentials are present" do
       client = Surveygizmo::Client.new(:username => "steven@somewhere.org", :password => "keyboardcat") 
@@ -9,6 +16,13 @@ describe Surveygizmo::Client do
     it "returns false if any credentials are missing" do
       client = Surveygizmo::Client.new(:username => "steven@somewhere.org")
       client.credentials?.should be_false
+    end
+  end
+
+  describe "#auth_query_hash" do
+    it "creates the correct auth query params" do
+      authorization = subject.send(:auth_query_hash)
+      authorization[:'user:md5'].should eq "steven@somewhere.org:39f8d5313141b1f2bade311ba571537e"
     end
   end
 end
