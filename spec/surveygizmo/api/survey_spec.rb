594466b8385a06f4deb053498a6af3f91f45cca9
Steven Chau <steven.chau@mac.com>
2012-09-03 20:12:42 -0700

    Define tests for survey

---
+++ spec/surveygizmo/api/survey_spec.rb
@@ -0,0 +1,45 @@
+require 'helper'
+
+describe Surveygizmo::API do
+  before do
+    @client = Surveygizmo::Client.new
+  end
+
+  describe "#surveys" do
+    before do
+     stub_get("/v1/survey").
+       to_return(:body => fixture("surveys.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.surveys
+      a_get("/v1/survey").
+        should have_been_made
+    end
+  end
+
+  describe "#polls" do
+    before do
+     stub_get("/v1/survey").
+       to_return(:body => fixture("surveys.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.polls
+      a_get("/v1/survey").
+        should have_been_made
+    end
+  end
+
+  describe "#survey" do
+    before do
+     stub_get("/v1/survey/1018301").
+       with(:query => {:metaonly=>"false"}).
+       to_return(:body => fixture("survey.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.survey(1018301)
+      a_get("/v1/survey/1018301").
+        with(:query => {:metaonly=>"false"}).
+        should have_been_made
+    end
+  end
+end
