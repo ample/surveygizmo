37d732412f74a403fa713f1373dbaf513a88ec23
Steven Chau <steven.chau@mac.com>
2012-09-03 21:01:11 -0700

    Add test for contacts

---
+++ spec/surveygizmo/api/contact_spec.rb
@@ -0,0 +1,31 @@
+require 'helper'
+
+describe Surveygizmo::API do
+  before do
+    @client = Surveygizmo::Client.new
+  end
+
+  describe "#contacts" do
+    before do
+     stub_get("/v1/survey/1018301/surveycampaign/673550/contact").
+       to_return(:body => fixture("contacts.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.contacts(1018301, 673550)
+      a_get("/v1/survey/1018301/surveycampaign/673550/contact").
+        should have_been_made
+    end
+  end
+
+  describe "#contact" do
+    before do
+     stub_get("/v1/survey/1018301/surveycampaign/673550/contact/1").
+       to_return(:body => fixture("contact.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.contact(1018301, 673550, 1)
+      a_get("/v1/survey/1018301/surveycampaign/673550/contact/1").
+        should have_been_made
+    end
+  end
+end
