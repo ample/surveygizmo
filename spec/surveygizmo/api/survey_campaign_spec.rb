5f0c6160119bbcecbcc3778d6ac6d61bb5fde1fe
Steven Chau <steven.chau@mac.com>
2012-09-03 20:52:30 -0700

    Add test for survey_campaign

---
+++ spec/surveygizmo/api/survey_campaign_spec.rb
@@ -0,0 +1,31 @@
+require 'helper'
+
+describe Surveygizmo::API do
+  before do
+    @client = Surveygizmo::Client.new
+  end
+
+  describe "#survey_campaigns" do
+    before do
+     stub_get("/v1/survey/1018301/surveycampaign").
+       to_return(:body => fixture("survey_campaigns.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.survey_campaigns(1018301)
+      a_get("/v1/survey/1018301/surveycampaign").
+        should have_been_made
+    end
+  end
+
+  describe "#survey_campaign" do
+    before do
+     stub_get("/v1/survey/1018301/surveycampaign/673550").
+       to_return(:body => fixture("survey_campaign.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+    it "requests the correct resource" do
+      @client.survey_campaign(1018301, 673550)
+      a_get("/v1/survey/1018301/surveycampaign/673550").
+        should have_been_made
+    end
+  end
+end
