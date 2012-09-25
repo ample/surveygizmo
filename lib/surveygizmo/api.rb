63e837a0d416f6244b6ce6cb8a29a95a6fba51e8
Steven Chau <steven.chau@mac.com>
2012-09-24 21:18:29 -0500

    Define object_from_response()

--- lib/surveygizmo/api.rb
+++ lib/surveygizmo/api.rb
@@ -17,5 +17,15 @@ module Surveygizmo
         instance_variable_set(:"@#{key}", options[key] || Surveygizmo.instance_variable_get(:"@#{key}"))
       end
     end
+
+    # @param klass [Class]
+    # @param request_method [Symbol]
+    # @param url [String]
+    # @param options [Hash]
+    # @return [Object]
+    def object_from_response(klass, request_method, url, options={})
+      response = send(request_method.to_sym, url, options)
+      klass.from_response(response)
+    end
   end
 end
