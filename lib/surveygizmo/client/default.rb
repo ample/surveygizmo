3b50d95c720e05bc472b19799772d1fb8f988f6c
Steven Chau <steven.chau@mac.com>
2012-09-26 14:26:25 -0700

    Remove duplicate default.rb

--- lib/surveygizmo/client/default.rb
+++
@@ -1,9 +0,0 @@
-module Surveygizmo
-  IDENTITY_MAP = Surveygizmo::IdentityMap unless defined? IDENTITY_MAP
-  class << self
-    # @return [Surveygizmo::IdentityMap]
-    def identity_map
-      IDENTITY_MAP
-    end
-  end
-end
